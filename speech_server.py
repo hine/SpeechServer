19734a585acdad66b56317c37eb7829705724b3f
Daisuke IMAI <hine.gdw@gmail.com>
2016-06-24 09:59:18 +0900

    open_jtalkでのsay機能実装

--- speech_server.py
+++ speech_server.py
@@ -11,7 +11,9 @@ http://opensource.org/licenses/mit-license.php
 import os
 import sys
 import time
+import datetime
 import json
+import subprocess
 
 import tornado.ioloop
 import tornado.web
@@ -21,10 +23,20 @@ class SpeechManager():
 	OPENJTALK_EXE = '/usr/bin/open_jtalk'
 	OPENJTALK_DIC_DIR = '/var/lib/mecab/dic/open-jtalk/naist-jdic'
 	OPENJTALK_VOICE_DIR = './voice'
+	MPLAYER = '/usr/bin/mplayer'
+	WORK_DIR = '/var/tmp'
 	def __init__(self):
 		pass
-	def say:
-		pass
+	def say(self, text):
+		datetime()
+		speech_file = SpeechManager.WORK_DIR + 'talk' + datetime.now().strftime('%Y/%m/%d') + '.wav'
+		open_jtalk_command = SpeechManager.OPENJTALK_EXE + ' -m ' +  SpeechManager.OPENJTALK_VOICE_DIR + '/mei/mei-happy.htsvoice -x ' + SpeechManager.OPENJTALK_DIC_DIR + ' -ow ' + speech_file
+		open_jtalk = subprocess.Popen(command, stdin=subprocess.PIPE)
+		open_jtalk.stdin.write(text)
+		open_jtalk.stdin.close()
+		open_jtalk.wait()
+		mplayer_command = SpeechManager.MPLAYER + ' ' + speech_file
+		subprocess.Popen(mplayer_command)
 
 #ここからTornadeでのWeb/WebSocketサーバーに関する定義
 class IndexHandler(tornado.web.RequestHandler):
@@ -91,6 +103,9 @@ if __name__ == '__main__':
         template_path=os.path.join(os.getcwd(),  'templates'),
         static_path=os.path.join(os.getcwd(),  'assets'),
     )
+
+	speech_manager = SpeechManager()
+	speech_manager.say('スピーチサーバーが起動しました')
 
     # Tornadoサーバー起動
     print('Starting Web/WebSocket Server...', end='')
