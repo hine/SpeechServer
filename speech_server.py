0194a7af2cd459088dac4b2892c46a6e67f39161
Daisuke IMAI <hine.gdw@gmail.com>
2016-06-24 10:55:34 +0900

    open_jtalk_commandの修正

--- speech_server.py
+++ speech_server.py
@@ -30,7 +30,7 @@ class SpeechManager():
 	def say(self, text):
 		speech_file = SpeechManager.WORK_DIR + 'talk' + datetime.datetime.now().strftime('%Y/%m/%d') + '.wav'
 		open_jtalk_command = SpeechManager.OPENJTALK_EXE + ' -m ' +  SpeechManager.OPENJTALK_VOICE_DIR + '/mei/mei-happy.htsvoice -x ' + SpeechManager.OPENJTALK_DIC_DIR + ' -ow ' + speech_file
-		open_jtalk = subprocess.Popen(command, stdin=subprocess.PIPE)
+		open_jtalk = subprocess.Popen(open_jtalk_command, stdin=subprocess.PIPE)
 		open_jtalk.stdin.write(text)
 		open_jtalk.stdin.close()
 		open_jtalk.wait()
