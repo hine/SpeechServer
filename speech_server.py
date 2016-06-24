8b02d511a739fbb5562102e4e10f8d1e95308530
Daisuke IMAI <hine.gdw@gmail.com>
2016-06-24 10:02:58 +0900

    スペースからタブに

--- speech_server.py
+++ speech_server.py
@@ -40,79 +40,79 @@ class SpeechManager():
 
 #ここからTornadeでのWeb/WebSocketサーバーに関する定義
 class IndexHandler(tornado.web.RequestHandler):
-    '''
-    通常のHTTPリクエストで/が求められた時のハンドラ
-    '''
-    @tornado.web.asynchronous
-    def get(self):
-        self.render("index.html")
+	'''
+	通常のHTTPリクエストで/が求められた時のハンドラ
+	'''
+	@tornado.web.asynchronous
+	def get(self):
+		self.render("index.html")
 
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
-    '''
-    WebSocketで/wsにアクセスが来た時のハンドラ
+	'''
+	WebSocketで/wsにアクセスが来た時のハンドラ
 
-    on_message -> receive data
-    write_message -> send data
-    '''
+	on_message -> receive data
+	write_message -> send data
+	'''
 
-    def open(self):
-        global md
-        global state
-        self.i = 0
-        self.callback = tornado.ioloop.PeriodicCallback(self._send_message, 50)
-        self.callback.start()
-        print('WebSocket opened')
-        # self.write_message(json.dumps({'message': 'connected'}))
+	def open(self):
+		global md
+		global state
+		self.i = 0
+		self.callback = tornado.ioloop.PeriodicCallback(self._send_message, 50)
+		self.callback.start()
+		print('WebSocket opened')
+		# self.write_message(json.dumps({'message': 'connected'}))
 
-    def check_origin(self, origin):
-        ''' アクセス元チェックをしないように上書き '''
-        return True
+	def check_origin(self, origin):
+		''' アクセス元チェックをしないように上書き '''
+		return True
 
-    def on_message(self, message):
-        global vc
-        global md
-        global ws
-        global state
+	def on_message(self, message):
+		global vc
+		global md
+		global ws
+		global state
 
-        received_data = json.loads(message)
-        print('got message:', received_data['command'])
-        if received_data['command'] == 'say':
-            print('Saying...', end='')
+		received_data = json.loads(message)
+		print('got message:', received_data['command'])
+		if received_data['command'] == 'say':
+			print('Saying...', end='')
 			# 発話に関する処理を入れる
-            print('done')
+			print('done')
 
-    def _send_message(self):
-        pass
-#        if len(vsidoconnect.message_buffer) > 0:
-#            self.write_message(vsidoconnect.message_buffer.pop(0))
+	def _send_message(self):
+		pass
+#		if len(vsidoconnect.message_buffer) > 0:
+#			self.write_message(vsidoconnect.message_buffer.pop(0))
 
-    def on_close(self):
-        self.callback.stop()
-        print('WebSocket closed')
+	def on_close(self):
+		self.callback.stop()
+		print('WebSocket closed')
 
 
 if __name__ == '__main__':
 
-    # アプリケーション割り当て
-    web_application = tornado.web.Application(
-        [
-            (r'/', IndexHandler),
-            (r'/ws', WebSocketHandler),
-        ],
-        template_path=os.path.join(os.getcwd(),  'templates'),
-        static_path=os.path.join(os.getcwd(),  'assets'),
-    )
+	# アプリケーション割り当て
+	web_application = tornado.web.Application(
+		[
+			(r'/', IndexHandler),
+			(r'/ws', WebSocketHandler),
+		],
+		template_path=os.path.join(os.getcwd(),  'templates'),
+		static_path=os.path.join(os.getcwd(),  'assets'),
+	)
 
 	speech_manager = SpeechManager()
 	speech_manager.say('スピーチサーバーが起動しました')
 
-    # Tornadoサーバー起動
-    print('Starting Web/WebSocket Server...', end='')
-    web_application.listen(8888)
-    print('done')
+	# Tornadoサーバー起動
+	print('Starting Web/WebSocket Server...', end='')
+	web_application.listen(8888)
+	print('done')
 
-    print('Open http://(SpeechServer_IP):8888/')
-    print('')
+	print('Open http://(SpeechServer_IP):8888/')
+	print('')
 
-    tornado.ioloop.IOLoop.instance().start()
+	tornado.ioloop.IOLoop.instance().start()
